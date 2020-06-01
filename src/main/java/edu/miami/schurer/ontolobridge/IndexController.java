dcc34f982d587983b8a4756deda4ea08f6c818e0
John Turner <jpt55@med.miami.edu>
2020-06-01 12:53:44 -0400

    Redirect index to swagger

---
+++ src/main/java/edu/miami/schurer/ontolobridge/IndexController.java
@@ -0,0 +1,16 @@
+package edu.miami.schurer.ontolobridge;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+@RestController
+public class IndexController {
+
+    //Redirect the root request to swagger page
+    @RequestMapping("/")
+    public void method(HttpServletResponse httpServletResponse) {
+        httpServletResponse.setHeader("Location", "swagger-ui.html");
+        httpServletResponse.setStatus(302);
+    }
+}
