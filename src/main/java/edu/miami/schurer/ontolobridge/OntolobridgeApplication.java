c87bd84be7e9c99c7111eb341a8fd0d1eb2ee2bc
John Turner <whinis@whinis.com>
2020-08-20 16:25:32 -0400

    Fix for 2 warnings in code that might possibly be causing database issues.

--- src/main/java/edu/miami/schurer/ontolobridge/OntolobridgeApplication.java
+++ src/main/java/edu/miami/schurer/ontolobridge/OntolobridgeApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
 @SpringBootApplication
@@ -15,5 +17,11 @@ public class OntolobridgeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OntolobridgeApplication.class, args);
+    }
+
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        return new ConcurrentTaskScheduler();
     }
 }
